b9165fa83cc6309e0c6df2b84d5d6cf955903b43
Alex Nagel <mail@alex-nagel.de>
2021-05-31 22:51:06 +0200

    Added LoadSaveVC.

---
+++ AVFoundation Test 8 Timer @ half periodLength/Controller/LoadSaveVC.swift
@@ -0,0 +1,48 @@
+//
+//  LoadSaveVC.swift
+//  AVFoundation Test 8 Timer @ half periodLength
+//
+//  Created by Alexander Nagel on 31.05.21.
+//
+
+import UIKit
+
+class LoadSaveVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Load / Save"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension LoadSaveVC: UITableViewDelegate, UITableViewDataSource {
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "snapshotCell", for: indexPath)
+        cell.textLabel?.text = "test"
+        return cell
+        
+    }
+    
+    
+    
+    
+}
